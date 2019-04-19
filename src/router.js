3f8df3338210d4e2d8d0b913f20cd88b1564893e
mrfong <therealshadix@gmail.com>
2019-04-19 22:51:44 +0300

    added endpoint for student page relates to #13

--- src/router.js
+++ src/router.js
@@ -4,9 +4,12 @@ const handler = require('./handlers.js');
 const router = (req, res) => {
   var url = req.url;
   if (url === '/') {
-handler.page("login", res)
- }
- else if (url.indexOf('public') !== -1) {
+    handler.page("login", res)
+  }
+  else if (url === "/student") {
+    handler.page("student", res)
+  }
+   else if (url.indexOf('public') !== -1) {
     handler.public(url, res);
   }
 
