890bfea3c1d4489adb99b09f6b9ba92a88006580
mrfong <therealshadix@gmail.com>
2019-04-19 19:41:43 +0300

    added extension types relates to #13

--- src/handlers.js
+++ src/handlers.js
@@ -1,2 +1,20 @@
 const fs = require('fs');
 const path = require('path');
+
+const extensionTypes = {
+  html: {
+    'Content-Type': 'text/html',
+  },
+  css: {
+    'Content-Type': 'text/css',
+  },
+  js: {
+    'Content-Type': 'application/javascript',
+  },
+  ico: {
+    'Content-Type': 'image/vnd.microsoft.icon',
+  },
+  json: {
+    'Content-Type': 'application/json',
+  },
+};
