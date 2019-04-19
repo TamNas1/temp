09b60ed95d39f55febdf5404bd17c0b1d175c155
Tamer Nasser <tamernasser@outlook.com>
2019-04-19 23:13:11 +0300

    Fixed linter error related #13

--- src/router.js
+++ src/router.js
@@ -1,14 +1,11 @@
-const url = require('url');
 const handler = require('./handlers.js');
 
 const router = (req, res) => {
-  var url = req.url;
+  const { url } = req;
   if (url === '/') {
-handler.page("index", res)
- }
- else if (url.indexOf('public') !== -1) {
+    handler.page('index', res);
+  } else if (url.indexOf('public') !== -1) {
     handler.public(url, res);
   }
-
 };
 module.exports = router;
