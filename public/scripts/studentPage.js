d12d21db973ede6efa50254c86485dbbd9290bc1
mrfong <therealshadix@gmail.com>
2019-04-19 18:12:05 +0300

    fixed multiplie javascript bugs relates to #1

--- public/scripts/studentPage.js
+++ public/scripts/studentPage.js
@@ -1,12 +1,11 @@
-function mytoggle(){
-  window.onload=size;
+window.onload=size;
 
-  function size(){
-    if (window.innerWidth<1050){
-        document.getElementById("logo").src="../../public/assests/minilogo.png";
+function size(){
+  if (window.innerWidth<1050){
+      document.getElementById("logo").src="../../public/assests/minilogo.png";
 
 }
-  }
+function mytoggle(){
 document.getElementById("sidebar").classList.toggle('active');
 }
 
