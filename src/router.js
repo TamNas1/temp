643b7e97e7bec8d4e614ea8f59d893e58b7b402f
mrfong <therealshadix@gmail.com>
2019-04-19 20:02:14 +0300

    added handlers in the router for public and html pages relates to #13

--- src/router.js
+++ src/router.js
@@ -1,9 +1,14 @@
+const url = require('url');
 const handler = require('./handlers.js');
 
 const router = (req, res) => {
   var url = req.url;
   if (url === '/') {
-    console.log('home page');
+handler.page(res)
+ }
+ else if (url.indexOf('public') !== -1) {
+    handler.public(url, res);
   }
+
 };
 module.exports = router;
