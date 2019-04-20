3406f2455f9cdb60c39f38ddaab2822cc8acfc73
mrfong <therealshadix@gmail.com>
2019-04-20 13:41:52 +0300

    added styling, handling, if the password is valid or empty, added more label styling relates to #17

--- public/scripts/loginPage.js
+++ public/scripts/loginPage.js
@@ -1,11 +1,30 @@
 var userVal = document.getElementById('username');
 var passVal = document.getElementById('password');
 var loginBtn = document.getElementById('signIn');
+var announceTxt  = document.getElementById('Announce');
 
 loginBtn.addEventListener("click",function(e){
+
+if( userVal.value.trim() == "" ) {
+  announceTxt.innerText = "Username cannot be empty"
+  announceTxt.style.color = "red";
+  userVal.style.border = "1px red solid";
+}
+else if( passVal.value.trim() == "" ) {
+  announceTxt.innerText = "Password cannot be empty"
+  announceTxt.style.color = "red";
+  userVal.style.border = "1px white solid";
+  passVal.style.border = "1px red solid";
+
+}
+else {
+  userVal.style.border = "1px white solid";
+  passVal.style.border = "1px white solid";
+
 e.preventDefault();
 fetchsignIn(loginBtn.value,signin);
 
+}
 
 })
 function fetchsignIn (value,cb){
@@ -27,5 +46,22 @@ cb(data);
 })
 }
 function signin(data){
-  console.log("the data we have is",data);
+  if( data === true) {
+    announceTxt.innerText = "Successfully logged in"
+    announceTxt.style.color = "green";
+    userVal.style.border = "1px green solid";
+    passVal.style.border = "1px green solid";
+
+    setTimeout(function() {
+      location.href = "/subjects";
+    }, 1500)
+  }
+  else if ( data === false ) {
+    announceTxt.innerText = "Invaid username/password"
+    announceTxt.style.color = "red";
+  }
+  else {
+    announceTxt.innerText = "Server error"
+    announceTxt.style.color = "red";
+  }
 }
