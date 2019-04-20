29a9cc98558bbc0d80aa5071a8d7a205dec8bfb0
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-20 18:32:32 +0300

    Added fetch to read sub subjects and updated dom dynamically after getting the data from the database relates #23

--- public/scripts/teacherPage.js
+++ public/scripts/teacherPage.js
@@ -1,9 +1,29 @@
+const containerSubSubjects = document.getElementById('subSubjects');
+
+fetch('/getSubSubjects')
+  .then(res => res.json())
+  .then((data) => {
+    updateDom(JSON.parse(JSON.stringify(data)))
+  });
+
+function updateDom(data) {
+  if( data ) {
+    data.map(function(item, index) {
+      var newLi = document.createElement("li");
+      var newA = document.createElement("a");
+      newA.innerText = data[index].name;
+
+      newLi.appendChild(newA);
+      containerSubSubjects.appendChild(newLi);
+    })
+  }
+}
+
 function size() {
   if (window.innerWidth < 1050) {
     document.getElementById('logo').src = '../../public/assests/minilogo.png';
   }
 }
-
 
 function mytoggle() {
   document.getElementById('wrapper').classList.toggle('active');
