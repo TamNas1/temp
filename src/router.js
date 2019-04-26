d7943e0dcdd4abdec77867b9a1b37eb2df052065
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-26 11:38:16 +0300

    Added checkauth route

--- src/router.js
+++ src/router.js
@@ -2,27 +2,17 @@ const handle = require('./handlers.js');
 
 const router = (req, res) => {
   const url = req.url;
-  if (url === '/') {
-    handle.page('login', res);
-  } else if (url === '/student') {
-    handle.page('student', res);
-  } else if (url === '/teacher') {
-    handle.page('teacher', res);
-  } else if (url === '/subjects') {
-    handle.page('subjects', res);
-  } else if (url === '/signin') {
-    handle.signIn(req, res);
-  } else if (url.indexOf('public') !== -1) {
-    handle.public(url, res);
-  } else if (url === '/getSubjects') {
-    handle.handleSubjects(res);
-  } else if (url === '/getHomeworks') {
-    handle.handleHomeworks(res);
-  } else if (url === '/getSubSubjects') {
-    handle.handleSubSubjects(res);
-  } else {
-    handle.page('404', res);
-  }
+  if (url === '/') handle.page('login', res);
+  else if (url === '/student') handle.page('student', res);
+  else if (url === '/teacher') handle.page('teacher', res);
+  else if (url === '/subjects') handle.page('subjects', res);
+  else if (url === '/signin') handle.signIn(req, res);
+  else if (url.indexOf('public') !== -1) handle.public(url, res);
+  else if (url === '/getSubjects') handle.handleSubjects(res);
+  else if (url === '/getHomeworks') handle.handleHomeworks(res);
+  else if (url === '/getSubSubjects') handle.handleSubSubjects(res);
+  else if (url === '/checkauth') handle.checkAuth(req, res);
+  else handle.page('404', res);
 };
 
 module.exports = router;
